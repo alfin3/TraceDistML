bb6d760d0f820b559bb2b2a795f95029f71a159f
AF <alexeifinski337@gmail.com>
2019-03-03 11:59:13 -0800

    added exit

--- infrastructure/generate-token.sh
+++ infrastructure/generate-token.sh
@@ -2,6 +2,8 @@
 
 # also see https://www.terraform.io/docs/providers/external/data_source.html#program
 
+set -e
+
 eval "$(jq -r '@sh "export PATH_TO_PRIV_KEY=\(.path_to_priv_key) USER=\(.user)  HOST=\(.host)"')"
 
 TOKEN_COMMAND=$(ssh -i $PATH_TO_PRIV_KEY -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $USER@$HOST sudo kubeadm token create --print-join-command)
